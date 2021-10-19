f78304b131009e217e6250148853fe129239d7cd
seyyed ali hossein nezhad <alihosseinnezhad3@gmail.com>
2021-10-19 12:22:05 +0330

    - update

--- app/src/main/java/com/example/iqoldemo/HomeActivity.kt
+++ app/src/main/java/com/example/iqoldemo/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.example.iqoldemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.iqoldemo.Adapter.ServiceTopItemsAdapter
+import com.example.iqoldemo.databinding.ServiceBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_layout)
+        val binding=  ServiceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.serviceRecyclerView.adapter = ServiceTopItemsAdapter(this)
     }
 }
