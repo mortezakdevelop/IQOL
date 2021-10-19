bfe4d017f6688fca7cdeed8a92a3dd1e0cc949b4
seyyed ali hossein nezhad <alihosseinnezhad3@gmail.com>
2021-10-19 15:58:25 +0330

    - update

--- app/src/main/java/com/example/iqoldemo/HomeActivity.kt
+++ app/src/main/java/com/example/iqoldemo/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
@@ -13,13 +14,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.iqoldemo.Adapter.ServiceTopItemsAdapter
 import com.example.iqoldemo.databinding.ServiceBinding
 import java.util.*
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 import kotlin.math.PI
+import kotlin.math.abs
 import kotlin.math.sin
 import kotlin.math.tanh
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setContentView(R.layout.test)
+//        setContentView(R.layout.register_enter_pin)
 //        setContentView(R.layout.home_layout)
         val binding = ServiceBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -67,7 +73,7 @@ class DesignCircles @JvmOverloads constructor(
             }
             for (i in 0..10) {
                 if (
-                    i in listOf(0, 1, 2,3)
+                    i in listOf(0, 1, 2, 3)
                 ) {
                     var x = ((i / 10f) - 0.1f) * width
                     var y = (exp2(i / 10f) + 0.1f) * height
@@ -77,7 +83,7 @@ class DesignCircles @JvmOverloads constructor(
             }
             for (i in 0..10) {
                 if (
-                    i !in listOf(7, 1, 5,3)
+                    i !in listOf(7, 1, 5, 3)
                 ) {
                     var x = ((i / 10f)) * width
                     var y = (exp2(i / 10f) + 0.2f) * height
@@ -86,14 +92,13 @@ class DesignCircles @JvmOverloads constructor(
                 }
             }
 
-            canvas.drawCircle(x,y*8,11f,paint)
-            canvas.drawCircle(x*3,y*8,15f,paint)
-            canvas.drawCircle(x*4,y*5,30f,paint)
-            canvas.drawCircle(x*2,y*6,11f,paint)
-            canvas.drawCircle(x*4.2f,y*5.5f,15f,paint)
-            canvas.drawCircle(x*13,y*8,26f,paint)
-            canvas.drawCircle(x*10,y*6,16f,paint)
-
+            canvas.drawCircle(x, y * 8, 11f, paint)
+            canvas.drawCircle(x * 3, y * 8, 15f, paint)
+            canvas.drawCircle(x * 4, y * 5, 30f, paint)
+            canvas.drawCircle(x * 2, y * 6, 11f, paint)
+            canvas.drawCircle(x * 4.2f, y * 5.5f, 15f, paint)
+            canvas.drawCircle(x * 13, y * 8, 26f, paint)
+            canvas.drawCircle(x * 10, y * 6, 16f, paint)
 
 
 //            for (i in 0 until 10) {
@@ -124,4 +129,60 @@ class DesignCircles @JvmOverloads constructor(
 //            }
         }
     }
+}
+
+class WaveFrame @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    var path = Path()
+    var count = 1f
+    var moveX = 0f
+    var paint = Paint().apply {
+        isAntiAlias = true
+        color = Color.BLUE
+        strokeWidth = 5f
+        style = Paint.Style.FILL
+    }
+
+    init {
+        Executors.newSingleThreadExecutor().execute {
+            var count = 0f
+            do {
+                Thread.sleep(40)
+                count += 0.01f
+                moveX += ((PI / 20f).toFloat())
+                var sin = abs(sin(count * PI)*sin(count * PI)).toFloat()
+                this.count = sin * 3f
+                postInvalidate()
+            } while (true)
+        }
+    }
+
+    fun Path.addWave(width: Float, height: Float) {
+        reset()
+        var dx = width / 100f
+        moveTo(0f, 0f)
+        for (i in 0..100) {
+            var x = dx * i
+            var y =
+                ((1 + sin(moveX + (x / width) * count * PI)) / 2f) * (height / 5f) + (0.8f * height)
+            lineTo(x, y.toFloat())
+        }
+        lineTo(width, 0f)
+        close()
+    }
+
+    init {
+        invalidate()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        path.addWave(width.toFloat(), height.toFloat())
+        canvas?.let {
+            canvas.drawPath(path, paint)
+        }
+    }
+
+
 }
