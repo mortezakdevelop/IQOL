8bdd8be32d99bcf4cd521fdc75c38a9c40458d0e
nafiseh <nafisehaliyavari@gmail.com>
2021-10-27 14:41:18 +0330

    .

---
+++ app/src/main/java/com/example/iqoldemo/IntroSlider0Activity2.kt
@@ -0,0 +1,11 @@
+package com.example.iqoldemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroSlider0Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_slider02)
+    }
+}
